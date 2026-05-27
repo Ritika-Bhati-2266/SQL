3e2360649aba369a46d2cf93b06c78813bfcc7cd
Ritika Bhati <ritika452044@gmail.com>
2026-05-27 22:35:09 +0530

    fix frontend + backend deploy

---
+++ Lecture_4.sql
@@ -0,0 +1,23 @@
+CREATE TABLE employee(
+	employee_id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	posiition VARCHAR(50),
+	department VARCHAR(50),
+	hire_date DATE,
+	salary NUMERIC(10,2)
+);
+SELECT * FROM employee;
+
+
+
+INSERT INTO employee(name, posiition, department, hire_date, salary)
+		VALUES('Ajit Sharma', 'Data Analyst', 'Data Science', '2022-05-15', 65000.00),
+	    ('Priya', 'HR manager','Human Resources','2019-03-10',82000.00);
+SELECT * FROM employee;
+
+ALTER TABLE employee
+RENAME COLUMN posiition to position;
+SELECT *FROM employee;
+
+
+		
