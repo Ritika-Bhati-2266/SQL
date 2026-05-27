87f40cf0bcb7e16f37f37d1ba05d529ae6788002
Ritika Bhati <ritika452044@gmail.com>
2026-05-27 23:23:10 +0530

    updated file

---
+++ 3) Deleting Rows, Columns from Table/Lecture_3.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE company2;
+CREATE TABLE employee2(
+	employee_id INT PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	position VARCHAR(50),
+	department VARCHAR(50),
+	hire_date DATE,
+	salary NUMERIC(10,2)
+);
+
+
+SELECT * FROM employee2;
+INSERT INTO employee2 (employee_id, name, position, department, hire_date, salary)
+		VALUES(101, 'Amit', 'Data Analyst','Data Science','2022-05-15', 65000.00),
+			(102, 'Priya', 'Sales Executive', 'Sales','2023-11-25',58000.00);
+SELECT * FROM employee2;
+
+DELETE FROM employee2
+WHERE department='Sales';
+SELECT * FROM employee2;
