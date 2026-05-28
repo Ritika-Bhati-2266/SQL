d01673c16759fde92d98f94ffe0f5b5f359c2b4b
Ritika Bhati <ritika452044@gmail.com>
2026-05-28 23:07:45 +0530

    updated file

---
+++ 10) How to import CSV File directly into SQL/Lecture_1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE employee3(
+employee_id INT PRIMARY KEY,
+first_name VARCHAR(20) NOT NULL,
+last_name VARCHAR(20) NOT NULL,
+email VARChAR(50),
+department VARCHAR(20),
+salary NUMERIC(10,2),
+joining_date DATE,
+age INT
+);
+SELECT * FROM employee3;
