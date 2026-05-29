0f4817b10e0a2cbf502fc822dd47897d3b6dab96
Ritika Bhati <ritika452044@gmail.com>
2026-05-29 16:14:47 +0530

    updated file

---
+++ 13) Logical Operators/Leccture_2.sql
@@ -0,0 +1,9 @@
+-- AND operator
+SELECT * FROM employee2;
+SELECT * FROM employee2 
+WHERE  age>=45 AND salary>=50000;
+
+-- OR operator
+SELECT *  FROM employee2;
+SELECT * FROM employee2
+WHERE age >=60 OR salary>=900000
---
+++ 13) Logical Operators/Lecture_1.sql
@@ -0,0 +1,4 @@
+-- AND operator
+SELECT * FROM employee2;
+SELECT * FROM employee2 
+WHERE  age>=45 AND salary>=50000;
