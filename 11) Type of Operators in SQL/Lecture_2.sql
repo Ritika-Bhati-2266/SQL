e93cf21cd8556a5a195a0ff95bc7b04b5254d882
Ritika Bhati <ritika452044@gmail.com>
2026-05-29 15:20:01 +0530

    updated file

---
+++ 11) Type of Operators in SQL/Lecture_2.sql
@@ -0,0 +1,19 @@
+-- Retrieve the first_name, salary, and calculate a 10% bonus on the salary.
+SELECT * FROM employee2;
+SELECT first_name, salary,(salary*0.10) AS Bonus
+FROM employee2;
+
+-- Calculate the annual salary and salary increment by 5% - showthe monthly new salary as .
+-- SELECT first_name, last_name, salary, 
+-- 	(salary*12)AS annual_salary,
+-- 	(salary*0.05) AS increment_salary,
+-- 	(salary+ salary*0.05) AS new_salary
+-- FROM employee2;
+
+-- Another method
+SELECT first_name, last_name, salary, 
+	(salary*12)AS annual_salary,
+	(salary*0.05) AS increment_salary,
+	(salary+ salary*0.05) AS new_salary,
+	(salary*1.05) AS new_salary2
+FROM employee2;
