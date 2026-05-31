316f52c90ebeb52698c4309ae9b75ae1781e65d1
Ritika Bhati <ritika452044@gmail.com>
2026-05-31 21:47:09 +0530

    updated file

---
+++ 22) Case Assignment/Lecture_1.sql
@@ -0,0 +1,42 @@
+DROP TABLE IF EXISTS products;
+CREATE TABLE products(
+	product_id SERIAL PRIMARY KEY,
+	product_name VARCHAR(50),
+	category VARCHAR(50),
+	price NUMERIC(10,2),
+	quantity INT,
+	added_date DATE,
+	discount_rate NUMERIC(5,2)	
+);
+INSERT INTO products(product_name, category, price, quantity, added_date, discount_rate)VALUES
+('Laptop', 'Electronics',75000.50,10,'2024-01-15',10.00),
+('Smartphone','Electronics',45000.99, 25, '2024-02-20',5.00),
+('Headphone','Accessories',1500.75,50,'2024-03-05',15.00),
+('Office Chair', 'Furniture', 5500.00, 20,'2023-12-01',20.00),
+('Desk', 'Furniture', 8000.00,15,'2023-11-20',12.00),
+('Monitor','Electronics', 12000.00,8,'2024-01-10',8.00),
+('Printer', 'Electronics',9500.50,5,'2023-03-18',10.00),
+('Mouse','AcceSsories',750.00,40,'2024-03-18', 10.00),
+('Keyboard', 'Accesories',1250.00,35,'2024-03-18',10.00),
+('Tablet','Electronics',30000,12,'2024-02-28',5.00);
+SELECT * FROM products;
+
+-- 1) In stock if quantity is 10 or more
+-- 2) Limited stock if quantity is between 5 and 9
+-- 3) Out of stock if quantity is less than 5
+SELECT product_name, quantity,
+		CASE
+			WHEN quantity>=10 THEN 'In stock'
+			WHEN quantity BETWEEN 5 AND 9 THEN 'Limited stock'
+			ELSE 'Out of stock'
+		END AS stock_status
+FROM products;
+
+
+SELECT product_name, quantity,
+		CASE
+			WHEN quantity>=10 THEN 'In stock'
+			WHEN quantity BETWEEN 6 AND 9 THEN 'Limited stock'
+			ELSE 'Out of stock soon'
+		END AS stock_status_
+FROM products;
