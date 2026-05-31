7a929a87296815a9e0ff554cad04e8bbb69bacda
Ritika Bhati <ritika452044@gmail.com>
2026-05-31 20:02:12 +0530

    updated file

---
+++ 19) Date & Time Functions/Lecture_5.sql
@@ -0,0 +1,51 @@
+DROP TABLE IF EXISTS products;
+CREATE TABLE products(
+	product_id SERIAL PRIMARY KEY,
+	product_name VARCHAR(50),
+	category VARCHAR(50),
+	price NUMERIC(10,2),
+	quantity INT,
+	added_date DATE,
+	discount_rate NUMERIC(5,2)	
+);
+INSERT INTO products(product_name, category, price, quantity, added_date, discount_rate)VALUES
+('Laptop', 'Electronics',75000.50,10,'2024-01-15',10.00),
+('Smartphone','Electronics',45000.99, 25, '2024-02-20',5.00),
+('Headphone','Accessories',1500.75,50,'2024-03-05',15.00),
+('Office Chair', 'Furniture', 5500.00, 20,'2023-12-01',20.00),
+('Desk', 'Furniture', 8000.00,15,'2023-11-20',12.00),
+('Monitor','Electronics', 12000.00,8,'2024-01-10',8.00),
+('Printer', 'Electronics',9500.50,5,'2023-03-18',10.00),
+('Mouse','AcceSsories',750.00,40,'2024-03-18', 10.00),
+('Keyboard', 'Accesories',1250.00,35,'2024-03-18',10.00),
+('Tablet','Electronics',30000,12,'2024-02-28',5.00);
+SELECT * FROM products;
+
+-- 1) NOW() - Get current Date and Time
+SELECT NOW() AS CURRENT_DATETIME;
+
+-- 2) CURRENT_DATE()- Get Current Date
+SELECT CURRENT_DATE AS today_date;
+
+
+SELECT added_date,current_Date,(CURRENT_DATE - added_date) AS Days_difference
+FROM products;
+
+-- 3) EXTRACT()- Extract Parts of a Date
+SELECT product_name,
+	EXTRACT(YEAR FROM added_date) AS Year_added,
+	EXTRACT(MONTH FROM added_date) AS Month_added,
+	EXTRACT(DAY FROM added_date) AS Day_added
+FROM products;
+
+-- 4) AGE() - Calaculate Age Between Dates
+-- Calculate the time difference between added_date and today's date
+SELECT product_name,
+	AGE(CURRENT_DATE, added_date) AS Age_since_added
+FROM products;
+
+-- 5) TO_CHAR()- Format Dates as Strings
+-- Format added_date in a custom format (DD-Mon-YYYY)
+SELECT product_name,
+	TO_CHAR(added_date, 'DD-Mon-YYYY') AS Age_since_added
+FROM products;
