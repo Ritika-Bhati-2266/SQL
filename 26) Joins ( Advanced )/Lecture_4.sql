a557df512c81715e87b2cdaa24ebd9fc9557d0a4
Ritika Bhati <ritika452044@gmail.com>
2026-06-01 23:10:13 +0530

    updated file

---
+++ 26) Joins ( Advanced )/Lecture_4.sql
@@ -0,0 +1,64 @@
+CREATE TABLE Employees3(
+	employee_id  SERIAL PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	department_id INT
+);
+INSERT INTO Employees3(employee_id ,first_name,last_name , department_id )VALUES
+(1,'Rahul', 'Sharma',101),
+(2,'Priya', 'Mehta', 102),
+(3,'Ankit', 'Verma', 103),
+(4,'Simran','Kaur',NULL),
+(5,'Aman','Singh',101);
+
+SELECT * FROM employees3;
+
+CREATE TABLE Departments(
+	department_id INT,
+	department_name VARCHAR(50)
+);
+INSERT INTO Departments(department_id, department_name)VALUES
+(101,'Sales'),
+(102, 'Marketing'),
+(103, 'IT'),
+(104, 'HR');
+
+SELECT * FROM Departments;
+
+SELECT e.employee_id,e.first_name, e.last_name, 
+		d.department_id, d.department_name
+FROM employees3 e
+FULL OUTER JOIN
+Departments d
+ON e.department_id=d.department_id;
+
+
+SELECT e.employee_id,e.first_name, e.last_name, 
+		d.department_id, d.department_name
+FROM employees3 e
+CROSS JOIN
+Departments d
+
+
+SELECT e1.first_name AS Employee_name1,
+		e2.first_name AS Employee_name2
+FROM employees3 e1 JOIN employees3 e2
+ON e1.department_id=e2.department_id AND e1.employee_id!=e2.employee_id;
+
+
+SELECT e1.first_name AS Employee_name1,
+		e2.first_name AS Employee_name2,
+		d.department_name
+FROM employees3 e1 JOIN employees3 e2
+ON e1.department_id=e2.department_id AND e1.employee_id!=e2.employee_id
+JOIN
+departments d
+ON
+e1.department_id=d.department_id;
+
+
+
+
+
+
+
